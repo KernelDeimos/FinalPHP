68c998bab8c17a36095cec549da163df1e2d2e5d
Eric Dube <eric.alex.dube@gmail.com>
2017-11-10 16:48:09 -0500

    Fix an "error" produced by PHP's "support" for anonymous functions

--- src/Functions/IO.php
+++ src/Functions/IO.php
@@ -19,7 +19,7 @@ class IO
                 // For each value in
                 $files,
                 // Map with the following functions
-                file_get_contents, array('\Symfony\Component\Yaml\Yaml', 'parse')
+                "file_get_contents", array('\Symfony\Component\Yaml\Yaml', 'parse')
             ),
             // Reduce function: Apply parsed YAML to each struct state
             L::ReverseArgs(
